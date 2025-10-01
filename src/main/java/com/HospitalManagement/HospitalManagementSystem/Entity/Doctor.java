5e4596f5210a114fb0fb8da570d82b34ba49baa1
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-10-02 01:21:12 +0530

    make dr entity

---
+++ src/main/java/com/HospitalManagement/HospitalManagementSystem/Entity/Doctor.java
@@ -0,0 +1,15 @@
+package com.HospitalManagement.HospitalManagementSystem.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "doctor_tbl")
+public class Doctor {
+}
