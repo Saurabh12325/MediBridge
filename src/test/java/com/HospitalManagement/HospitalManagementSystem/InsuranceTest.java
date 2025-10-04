c57e3987ecf51c847417bd5638dadcda96789965
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-10-04 16:01:02 +0530

    changes

--- src/test/java/com/HospitalManagement/HospitalManagementSystem/InsuranceTest.java
+++ src/test/java/com/HospitalManagement/HospitalManagementSystem/InsuranceTest.java
@@ -2,6 +2,7 @@ package com.HospitalManagement.HospitalManagementSystem;
 
 import com.HospitalManagement.HospitalManagementSystem.Entity.Insurance;
 import com.HospitalManagement.HospitalManagementSystem.Entity.Patient;
+import com.HospitalManagement.HospitalManagementSystem.Service.AppointmentService;
 import com.HospitalManagement.HospitalManagementSystem.Service.InsuranceService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,9 @@ public class InsuranceTest {
 
     @Autowired
     private InsuranceService insuranceService;
+
+    @Autowired
+    private AppointmentService appointmentService;
     @Test
     public void testInsurance(){
         Insurance insurance = Insurance.builder()
@@ -24,4 +28,7 @@ public class InsuranceTest {
        Patient patient =  insuranceService.assignInsurancetoPatient(insurance,1L);
        System.out.println(patient);
     }
+
+    @Test
+    p
 }
