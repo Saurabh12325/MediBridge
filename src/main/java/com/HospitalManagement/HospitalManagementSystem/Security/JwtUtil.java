7869976e6e07ebd0aa574461f4aeddb37c4874d6
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-10-05 23:44:42 +0530

    changes

--- src/main/java/com/HospitalManagement/HospitalManagementSystem/Security/JwtUtil.java
+++ src/main/java/com/HospitalManagement/HospitalManagementSystem/Security/JwtUtil.java
@@ -1,8 +1,12 @@
 package com.HospitalManagement.HospitalManagementSystem.Security;
 
 
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class JwtUtil {
@@ -11,6 +15,8 @@ public class JwtUtil {
 //    we need three things to generate a jwt token secretKry , header , Payload
       @Value("${jwt.secret}")
       private String jwtSecretKey;
-
+  private SecretKey getSecretKey() {
+    return Keys.hmacShaKeyFor(jwtSecretKey.getBytes(StandardCharsets.UTF_8));
+  }
 
 }
