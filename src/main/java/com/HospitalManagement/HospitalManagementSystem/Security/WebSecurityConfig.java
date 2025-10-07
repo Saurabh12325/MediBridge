341c432c30f4bbe26de4ec706a57f17752b3cb7b
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-10-08 02:24:54 +0530

    add oAuth

--- src/main/java/com/HospitalManagement/HospitalManagementSystem/Security/WebSecurityConfig.java
+++ src/main/java/com/HospitalManagement/HospitalManagementSystem/Security/WebSecurityConfig.java
@@ -1,19 +1,28 @@
 package com.HospitalManagement.HospitalManagementSystem.Security;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class WebSecurityConfig{
 
     private final JwtAuthFilter authFilter;
@@ -26,7 +35,16 @@ public class WebSecurityConfig{
                 .authorizeHttpRequests(auth ->auth
                                 .requestMatchers("/public/**","/auth/**").permitAll()
                                 .anyRequest().authenticated())
-                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+                 .oauth2Login(oauth2 -> oauth2.failureHandler(
+                         new AuthenticationFailureHandler() {
+                             @Override
+                             public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                                 log.error("aAuthError : {}", exception.getMessage());
+                             }
+                         }
+                 ));
+        ;
 
         return http.build();
 
