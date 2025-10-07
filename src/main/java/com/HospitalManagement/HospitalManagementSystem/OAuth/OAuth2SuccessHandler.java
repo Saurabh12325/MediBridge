30e7572809d28a5f53b35e02f2c5f1e5eaa17f0f
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-10-08 02:41:17 +0530

    add oAuth

--- src/main/java/com/HospitalManagement/HospitalManagementSystem/OAuth/OAuth2SuccessHandler.java
+++ src/main/java/com/HospitalManagement/HospitalManagementSystem/OAuth/OAuth2SuccessHandler.java
@@ -1,4 +1,19 @@
 package com.HospitalManagement.HospitalManagementSystem.OAuth;
 
-public class OAuth2SuccessHandler {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+@Component
+@RequiredArgsConstructor
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+    }
 }
