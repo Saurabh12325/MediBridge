a2dc715a96cb9b62eb052638f258e6ac37788ab8
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-10-11 17:06:36 +0530

    Granular Authorization

--- src/main/java/com/HospitalManagement/HospitalManagementSystem/Security/WebSecurityConfig.java
+++ src/main/java/com/HospitalManagement/HospitalManagementSystem/Security/WebSecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
@@ -31,9 +32,10 @@ public class WebSecurityConfig{
 
     private final JwtAuthFilter authFilter;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
+//    private final  HandlerExceptionResolver handlerExceptionResolver;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http ) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerExceptionResolver handlerExceptionResolver) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -45,12 +47,19 @@ public class WebSecurityConfig{
                 )
                 .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
                  .oauth2Login(oauth2 -> oauth2
-                         .failureHandler( (request, response, exception) ->
-                                 log.error("aAuthError : {}", exception.getMessage())
-                         )
+                         .failureHandler( (request, response, exception) -> {
+                             handlerExceptionResolver.resolveException(request, response, null, exception);
+                             log.error("aAuthError : {}", exception.getMessage());
+
+                         })
                  .successHandler(oAuth2SuccessHandler)
 
-                 );
+                 )
+                .exceptionHandling(exceptionHandlingConfig-> exceptionHandlingConfig.accessDeniedHandler((request, response, accessDeniedException) -> {
+                    handlerExceptionResolver.resolveException(request, response, null, accessDeniedException);
+                    log.error("Access denied: {}", accessDeniedException.getMessage());
+
+                }))
         ;
 
         return http.build();
